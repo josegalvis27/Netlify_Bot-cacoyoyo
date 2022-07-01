f4866ac041709af26ecbb1653b9bbaf8c644f9c8
jgalvis414 <ygalvis414@gmail.com>
2022-06-30 23:46:49 -0400

    feat: add fn-delete

---
+++ functions/fn-delete-cart-with-id/fn-delete-cart-with-id.js
@@ -0,0 +1,27 @@
+let connectDB = require("../../database/database");
+exports.handler = async (event, context) => {
+    let {
+        httpMethod: method,
+        queryStringParameters:parametro,
+        body
+    } = event;
+    if (method == 'GET') {
+        try {    
+            let id = Number(parametro.id)
+            let client = await connectDB();
+            const colCarts = client.db('databasetest').collection('cartsUsers');
+            const findResult = await colCarts.findOneAndDelete({'idTlgUser' : id})
+            //console.log(findResult)
+            return {
+                statusCode: 200,
+                body: JSON.stringify(findResult),
+                // // more keys you can return:
+                // headers: { "headerName": "headerValue", ... },
+                // isBase64Encoded: true,
+            }
+        } catch (error) {
+            return { statusCode: 500, body: error.toString() }
+        }
+    }
+
+}
