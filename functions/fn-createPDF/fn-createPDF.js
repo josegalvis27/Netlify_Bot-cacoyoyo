1c117cd5731c2df49395e5c8e13e6ae22a54acc6
jgalvis414 <ygalvis414@gmail.com>
2022-07-06 11:36:32 -0400

    feat

--- functions/fn-createPDF/fn-createPDF.js
+++ functions/fn-createPDF/fn-createPDF.js
@@ -11,7 +11,7 @@ exports.handler = async (event, context) => {
         try {
             let newBody1 = JSON.parse(event.body)
             console.log(newBody1)
-            let path2 = path.resolve('/utils/invoice.pdf')
+            let path2 = './utils/invoice.pdf'
 
             createInvoice(newBody1, path2)
             
@@ -28,7 +28,7 @@ exports.handler = async (event, context) => {
             }
 
             function generateHeader(doc) {
-                let pathLogo = path.resolve('/utils/logo.png')
+                let pathLogo = './utils/logo.png'
                 doc
                     .image(pathLogo, 50, 45, { width: 50 })
                     .fillColor("#444444")
